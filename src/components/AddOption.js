8dbb31ff6d106944427f551e0498bd10f147f0ae
wiel.leo <wiel.leo@gmail.com>
2020-04-21 22:57:15 +0200

    Almost end of Section 6

---
+++ src/components/AddOption.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addOption = this.addOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
+    
+    addOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
+
+        this.setState(() => ({ error }));
+
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
+        
+    }
+
+    render () {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.addOption}>
+                    <input type="text" name="option" />
+                    <button>Add option</button> 
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddOption;
---
+++ src/components/Options.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Option from './Option';
+
+const Options = (props) => {
+    return (
+        <div>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
+            <ol>
+                {
+                    props.options.map((option) => {
+                        return <Option
+                            key={option}
+                            option={option}
+                            handleDeleteOption={props.handleDeleteOption}
+                        />
+                    })
+                }
+            </ol>
+            <button onClick={props.handleDeleteOptions}>Remove all options</button>
+        </div>
+    );
+};
+
+export default Options;
