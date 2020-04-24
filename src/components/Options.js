ee5804459c22e48784142e5722d85de26d0f3a36
wiel.leo <wiel.leo@gmail.com>
2020-04-24 19:30:19 +0200

    end of section 8

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,19 +3,27 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
-        <ol>
-            {
-                props.options.map((option) => {
-                    return <Option
-                        key={option}
-                        option={option}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                })
-            }
-        </ol>
-        <button onClick={props.handleDeleteOptions}>Remove all options</button>
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your options</h3>
+            <button 
+                className="button button--link"
+                onClick={props.handleDeleteOptions}
+            >
+                Remove all
+            </button>        
+        </div>
+
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}        
+        {
+            props.options.map((option, index) => (
+                <Option
+                    key={option}
+                    option={option}
+                    count={index+1}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
+            ))
+        }
     </div>
 );
 
